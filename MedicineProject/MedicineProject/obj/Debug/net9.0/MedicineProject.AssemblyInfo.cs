64e62f98ebc91fd3e71e16012013149cb4ac09fb
tranvutruonghuy <167849255+tranvutruonghuy@users.noreply.github.com>
2025-01-03 16:23:48 +0700

    1/3/2025 backup After changing locations of folders

--- MedicineProject/MedicineProject/obj/Debug/net9.0/MedicineProject.AssemblyInfo.cs
+++ MedicineProject/MedicineProject/obj/Debug/net9.0/MedicineProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedicineProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88fbdf52911befdfbb21e27d440ba3db9de69eb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d49d080ee47f277e2266bd84cc03fad3cd8da527")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedicineProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedicineProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
